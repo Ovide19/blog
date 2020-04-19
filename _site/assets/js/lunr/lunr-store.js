b049127b325a94b7974a30ccad6dcdbc8e7d40b3
sheldon <sheldon@datacloud.com>
2020-04-20 01:09:48 +0200

    committing my jekyll theme

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Hunting digital easter eggs",
+        "excerpt":"With the COVID-19 lockdown slightly complicating this year’s easter egg hunt, why not turn to digital easter eggs for fun? If you are into electronic music, you probably heard of Aphex Twin’s images hidden into some of the tracks of his 1999 “Windowlicker” album, such as the grinning face buried...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/equation/",
+        "teaser": "http://localhost:4000/assets/images/equation_overlay.jpg"
+      },{
+        "title": "Mapping COVID-19 deaths using Python and Folium",
+        "excerpt":"Next you can update your site name, avatar and other options using the _config.yml file in the root of your repository (shown below). The easiest way to make your first post is to edit this one. Go into /_posts/ and update the Hello World markdown file. For more instructions head...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/covid/",
+        "teaser": null
+      }]
